1a2ceb180c36418402e437458a6ccfa3ee119a60
Mányik Bence <h161847@stud.u-szeged.hu>
2025-04-09 22:09:07 +0200

    Added home, profile and menu

--- RemindR/src/app/app.component.ts
+++ RemindR/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {MenuComponent} from './shared/menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
