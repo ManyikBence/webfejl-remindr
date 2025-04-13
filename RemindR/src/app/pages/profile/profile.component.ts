91599de0cdad17b3b6492edec22357c123053fb2
Mányik Bence <h161847@stud.u-szeged.hu>
2025-04-13 23:51:22 +0200

    Working

--- RemindR/src/app/pages/profile/profile.component.ts
+++ RemindR/src/app/pages/profile/profile.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import {MatCard} from '@angular/material/card';
+import {MatDivider} from '@angular/material/list';
+import {MatFormField, MatInput} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  imports: [
+    MatCard,
+    MatDivider,
+    MatFormField,
+    MatInput,
+    FormsModule,
+    MatButton
+  ],
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+  isEditing = false;
 
+  user = {
+    name: 'Kovács Péter',
+    email: 'peter.kovacs@gmail.com',
+    about: 'Szenvedélyes fejlesztő és sorozatfüggő.',
+    registered: '2023.04.12',
+    avatar: 'KP'
+  };
+
+  toggleEdit() {
+    this.isEditing = !this.isEditing;
+  }
+
+  saveChanges() {
+    this.isEditing = false;
+  }
 }
