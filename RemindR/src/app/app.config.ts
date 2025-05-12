1dc014d9005efd953f13145a06bfe6646cf21fc4
Mányik Bence <h161847@stud.u-szeged.hu>
2025-05-12 11:05:31 +0200

    Firebase added

--- RemindR/src/app/app.config.ts
+++ RemindR/src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "remindr-web-c9996", appId: "1:747113477717:web:5741e465781641b194c6dc", storageBucket: "remindr-web-c9996.firebasestorage.app", apiKey: "AIzaSyAMpYS5ez-kcrLRNjD_DqQHpedwkZr-mPc", authDomain: "remindr-web-c9996.firebaseapp.com", messagingSenderId: "747113477717" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
